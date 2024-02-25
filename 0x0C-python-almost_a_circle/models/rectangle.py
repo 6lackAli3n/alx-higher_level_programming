b119514ef4cce500b24898c232778b44b5ebfc77
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-02-25 18:57:00 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,10 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def area(self):
+        """Calculate and return the area of the rectangle"""
+        return self.width * self.height
+
     @property
     def width(self):
         """Retrieve the width of the rectangle"""
