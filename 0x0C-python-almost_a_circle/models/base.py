5f313e0543c01328e96c78a10dfff76382351e34
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-02-27 01:30:41 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,11 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Constructor method for Base class"""
+        """Initialize a new Base.
+
+        Args:
+        id (int): The identity of the new Base.
+        """
         if id is not None:
             self.id = id
         else:
@@ -18,17 +22,17 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
-        if list_objs is None:
-            list_objs = []
-            filename = cls.__name__ + ".json"
-            with open(filename, "w") as file:
-                json_string = cls.to_json_string(
-                        [obj.to_dictionary() for obj in list_objs])
-                file.write(json_string)
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                file.write(Base.to_json_string(list_dicts))
