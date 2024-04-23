151ccbdbce213eb003c8af77dc10f55c65064221
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-04-23 17:26:18 +0100

    java

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function secondBiggest(numbers) {
+	const sortedNumbers = numbers.sort((a, b) => b - a);
+	if (sortedNumbers.length <= 1) {
+		return 0;
+	} else {
+		return sortedNumbers[1];
+	}
+}
+
+const args = process.argv.slice(2).map(Number);
+console.log(secondBiggest(args));
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * A js script that only prints the first argument passed to it on the
+ * terminal
+ */
+const value = process.argv[2];
+if (value === undefined) {
+	console.log('No argument');
+} else {
+	console.log(value);
+}
