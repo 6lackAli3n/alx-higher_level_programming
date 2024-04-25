2c6ec85b5f296c2b0aa0937ba5169c3cbd387534
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-04-25 02:20:43 +0100

    rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor (x, y) {
-		if (x > 0 && y > 0) {
-			this.width = x;
-			this.height = y;
-		}
-	}
-}
+  constructor (x, y) {
+    if (x > 0 && y > 0) {
+      this.width = x;
+      this.height = y;
+    }
+  }
+};
