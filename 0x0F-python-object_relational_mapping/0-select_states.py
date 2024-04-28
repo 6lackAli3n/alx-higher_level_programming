0b2935e8bc7250fc48afc03b1cdc14b2281f9d5b
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-04-28 02:13:54 +0100

    state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+"""
 
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
-    states = cur.fetchall()
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    for state in states:
-        print(state)
+    # Get database connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to select all states
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all rows and print them
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
