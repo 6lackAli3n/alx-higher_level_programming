4b5579daf1fb067b433f6ba3fa43fae0be618d9e
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-05-10 03:56:18 +0100

    body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Send a request to the provided URL and display the size of the body of the response in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+curl -s "${1}" | wc -c
