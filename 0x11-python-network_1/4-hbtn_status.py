500692dbd5bb10776e1ba59ce465b223f6194fb4
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-05-10 20:21:05 -0700

    email

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Script to fetch status from a URL using the requests library
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
