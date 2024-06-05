6d6b573bc61746e9e1157682dbe170d6b52a55ff
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-05 02:58:33 +0100

    all done

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+	if (error) {
+		console.error('Error:', error);
+		return;
+	}
+
+	const film = JSON.parse(body);
+	const characterUrls = film.characters;
+
+	function getCharacterName(characterUrl) {
+		return new Promise((resolve, reject) => {
+			request.get(characterUrl, (error, response, body) => {
+				if (error) {
+					reject(error);
+				} else {
+					const character = JSON.parse(body);
+					resolve(character.name);
+				}
+			});
+		});
+	}
+
+	const characterPromises = characterUrls.map(getCharacterName);
+
+	Promise.all(characterPromises)
+	.then(characterNames => {
+		characterNames.forEach(name => console.log(name));
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+});
